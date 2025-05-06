8c82091b34a351152a653768ae144471656f06ba
Ilya Voronin <ivoronin@gmail.com>
2025-05-06 18:47:28 +0000

    fixed service host patterns

--- service.go
+++ service.go
@@ -9,16 +9,16 @@ var endpointPatterns = []*regexp.Regexp{
 	// Most Amazon Web Services offer a Regional endpoint that you can use to make your requests.
 	// In general, these endpoints support IPv4 traffic and they use the following syntax:
 	// protocol://service-code.region-code.amazonaws.com
-	regexp.MustCompile(`^[a-z]+://([a-z0-9-]+)\.[a-z0-9-]+\.amazonaws\.com(\.cn)?$`),
+	regexp.MustCompile(`^([a-z0-9-]+)\.[a-z0-9-]+\.amazonaws\.com(\.cn)?$`),
 
 	// Some AWS services offer dual stack endpoints, so that you can access them using
 	// either IPv4 or IPv6 requests. In general, the syntax of a dual stack endpoint is as follows:
 	// protocol://service-code.region-code.api.aws
-	regexp.MustCompile(`^[a-z]+://([a-z0-9-]+)\.[a-z0-9-]+\.api\.aws$`),
+	regexp.MustCompile(`^([a-z0-9-]+)\.[a-z0-9-]+\.api\.aws$`),
 
 	// However, Amazon S3 uses the following syntax for its dual stack endpoints:
 	// protocol://service-code.dualstack.region-code.amazonaws.com
-	regexp.MustCompile(`^[a-z]+://([a-z0-9-]+)\.dualstack\.[a-z0-9-]+\.amazonaws\.com(\.cn)?$`),
+	regexp.MustCompile(`^([a-z0-9-]+)\.dualstack\.[a-z0-9-]+\.amazonaws\.com(\.cn)?$`),
 
 	// Well, some endpoints do not fit into standard patterns
 	regexp.MustCompile(`(s3|es)\.amazonaws\.com(\.cn)?$`),
